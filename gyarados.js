44ea978338fe0593bf741bf18c5918efa05b4dbc
Aron Winston <aronwinston2792@gmail.com>
2018-12-11 17:55:47 -0500

    added all pokemon and stats

--- gyarados.js
+++ gyarados.js
@@ -1,20 +1,17 @@
 'use strict'
 
-axios.get("https://pokeapi.co/api/v2/pokemon/mewtwo/")
+axios.get("https://pokeapi.co/api/v2/pokemon/gyarados/")
 .then(response => {
 
     let data = response.data;
     console.log(data);
 
-    let aronClass = document.querySelector(".mewtwo");
+    let aronClass = document.querySelector(".gyarados");
     aronClass.innerHTML= `<p class="hp">HP: ${data.stats[5].base_stat}</p><p class="attack">ATTACK: ${data.stats[4].base_stat}
                             </p><p class="defense">DEFENSE: ${data.stats[3].base_stat}</p>`;
 
 
 
-
-    
-    
-
-
+ 
+                            
 });
